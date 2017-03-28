f3e86ad59ea81ab5524c5ee19afcabc17ea9c4c6
Eric Maranga <ekirira22@gmail.com>
2017-03-28 23:48:44 +0300

    Did dynamic routing and refactored our projects by creating another route to hold the delete and update components instead of them being in the index. Also added links to link the dynamic pages Also installed bootstrap which gave the new page a whole new look like a real ember app The projects is with a few bugs though, the new route displays the image twice with the delete button again :(

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,21 +9,6 @@ export default Ember.Route.extend({
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
-    },
-    update(rental, params) {
-  Object.keys(params).forEach(function(key) {
-    if(params[key]!==undefined) {
-      rental.set(key,params[key]);
-    }
-  });
-  rental.save();
-  this.transitionTo('index');
-},
-    destroyRental(rental){
-      rental.destroyRecord();
-      this.transitionTo('index');
-      //when you destroy a record it gets chucked from the database that you had stored it in, in this case the data storage
-      //we were using was ember fire where we uploaded a rentals.json file which acts as a data store.
     }
   }
 });
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  models(params){
+    return this.store.findRecord('rental', params.rental_id);
+  },
+  actions :  {
+    update(rental, params) {
+        Object.keys(params).forEach(function(key) {
+          if(params[key]!==undefined) {
+            rental.set(key,params[key]);
+      }
+    });
+    rental.save();
+    this.transitionTo('index');
+    },
+      destroyRental(rental){
+        rental.destroyRecord();
+        this.transitionTo('index');
+        //when you destroy a record it gets chucked from the database that you had stored it in, in this case the data storage
+        //we were using was ember fire where we uploaded a rentals.json file which acts as a data store.
+      }
+  }
 });
