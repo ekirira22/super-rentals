92bc6001c84fcb7ed5ecad9804c7b8e1872edb76
Eric Maranga <ekirira22@gmail.com>
2017-03-30 12:42:41 +0300

    We added a new component a new review component that allows th user to add a new review instead of using the hard code data that we were using in the previous commit and we called it in the index.hbs

---
+++ app/components/new-review.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewReview: false,
+  actions: {
+    reviewFormShow() {
+      this.set('addNewReview', true);
+    },
+    saveReview() {
+      var params = {
+        author: this.get('author'),
+        rating: this.get('rating'),
+        content: this.get('content')
+      };
+      this.set('addNewReview', false);
+      this.sendAction('saveReview', params);
+    },
+    hideReview() {
+      this.set('addNewReview', false)
+    }
+  }
+});
