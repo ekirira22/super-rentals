548fb356118518465b09bbe382ca4f958ffd3674
Eric Maranga <ekirira22@gmail.com>
2017-03-30 11:39:06 +0300

    We now created multiple models using the RSVP hash and we were able to return multiple models in on model hook which is called on the index.hbs and we are able to see the hard coded reviews Next we will now add a form for the reviews instead of using hard coded data

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'), //this 3 lines of code now replaced "return this.store.findAll('rental');"
+      reviews: this.store.findAll('review') //in order to return multiple models, now we use Ember.RSVP.hash({}) ....This lines of code are called promises.
+    });
   },
-  actions : {
-    saveRental3(params){
+  actions: {
+    saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
