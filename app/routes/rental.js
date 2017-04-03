7c9143c32f2da8cddb45420973d3e65a494c1210
Eric Maranga <ekirira22@gmail.com>
2017-04-03 18:03:17 +0300

    Refactored the New Review Functionality where the user is able to add a new review to each rental and leave a comment

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,24 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  models(params){
+  models(params) {
     return this.store.findRecord('rental', params.rental_id);
   },
-  actions :  {
+  actions: {
     update(rental, params) {
-        Object.keys(params).forEach(function(key) {
-          if(params[key]!==undefined) {
-            rental.set(key,params[key]);
-      }
-    });
-    rental.save();
-    this.transitionTo('index');
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+          rental.set(key, params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
     },
-      destroyRental(rental){
-        rental.destroyRecord();
-        this.transitionTo('index');
-        //when you destroy a record it gets chucked from the database that you had stored it in, in this case the data storage
-        //we were using was ember fire where we uploaded a rentals.json file which acts as a data store.
-      }
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      var rental = params.rental;
+      rental.get('reviews').addObject(newReview);
+      newReview.save().then(function() {
+        return rental.save();
+      });
+      this.transitionTo('rental', rental);
+    },
+    destroyRental(rental) {
+      var review_deletions = rental.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return rental.destroyRecord();
+      });
+      this.transitionTo('index');
+    },
+    destroyReview(review) {
+      review.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
+
+//when you destroy a record it gets chucked from the database that you had stored it in, in this case the data storage
+//we were using was ember fire where we uploaded a rentals.json file which acts as a data store.
